5a1469bdf2a4eff60c29127de2cb8e63c75ea9b0
irina shkola <iri6kass@mail.ru>
2023-04-27 20:11:13 +0300

    Третья задача

---
+++ Задача4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+Console.Write ("Введите число A");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число B");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число C");
+int numC = Convert.ToInt32(Console.ReadLine());
+if (numA>numB&&numB>numC)
+{
+    Console.Write($"max={numA}");
+}
+else if (numA<numB&&numB>numC)
+{
+    Console.Write($"max={numB}");
+}
+else if (numB<numC&&numC>numA)
+{
+    Console.Write($"max={numC}");
+}
---
+++ Задача6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным(делится ли оно без остатка)
+// 4-> да
+// -3 -> нет
+// 7 -> нет
+Console.Write("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(number/2);
+if (number%2 == 0)
+{
+    Console.Write("да");
+}
+else
+{
+    Console.Write("нет");
+}
