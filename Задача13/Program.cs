847e52007e3470feab6d9b5900c8e0cf6a940a42
irina shkola <iri6kass@mail.ru>
2023-04-29 06:44:07 +0300

    Второе ДЗ

---
+++ Задача13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+    Console.WriteLine("Введите трехзначное число : ");
+    string number = Console.ReadLine();
+    int numberLenght = number.Length;
+
+    if (numberLenght > 2)
+    {
+    Console.WriteLine($"Третья цифра этого числа это: {number[2]}");
+    }
+    else
+    {
+    Console.WriteLine("Этой цифры нет");
+    }
+
+
